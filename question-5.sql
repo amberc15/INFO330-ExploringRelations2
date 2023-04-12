f47d9796fc696bb9dcdc4f5b6e0ffd5bfd79e503
amberc15 <ambercarbajal33@gmail.com>
2023-04-11 23:56:41 -0700

    HW 2

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,6 @@
--- What was the most purchased track of 2013?
+--(question-5.sql) What was the most purchased track of 2013?
+
+SELECT tracks.name as Track,
+COUNT (*) as NumPurchases FROM invoice_items 
+JOIN invoices on invoice_items.InvoiceId = invoices.InvoiceId
+JOIN tracks ON invoice_items.TrackId;
